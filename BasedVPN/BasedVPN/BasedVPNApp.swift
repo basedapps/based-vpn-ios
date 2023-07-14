78babe9267aa45b60495601bf1eff79b35c7155f
Lika <lika@solarlabs.ee>
2023-07-14 13:13:47 +0300

    Create a project

---
+++ BasedVPN/BasedVPN/BasedVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasedVPNApp.swift
+//  BasedVPN
+//
+//  Created by Lika Vorobeva on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BasedVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
