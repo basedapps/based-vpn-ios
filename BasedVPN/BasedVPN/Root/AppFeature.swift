1138265bcb07261c6c6d9221649b7e4e91f8232b
Lika Vorobeva <lika@solarlabs.ee>
2023-10-30 18:12:38 +0200

    Stop tunnel on a banned device

--- BasedVPN/BasedVPN/Root/AppFeature.swift
+++ BasedVPN/BasedVPN/Root/AppFeature.swift
@@ -6,7 +6,9 @@
 
 import ComposableArchitecture
 
-struct AppFeature: Reducer {
+// MARK: - AppFeature
+
+struct AppFeature {
     struct State: Equatable {
         var viewState: ViewState<HomeFeature.State, ConnectionError>
         var homeState: HomeFeature.State
@@ -19,11 +21,17 @@ struct AppFeature: Reducer {
         case home(HomeFeature.Action)
         
         case deviceBanned
+        case tunnelStopped
         case deviceNotEnrolled
     }
 
     @Dependency(\.deviceClient) var deviceClient
+    @Dependency(\.tunnelManagerSubscriber) var tunnelManagerSubscriber
+}
 
+// MARK: - Reducer
+
+extension AppFeature: Reducer {
     var body: some Reducer<State, Action> {
         Scope(state: \.homeState, action: /Action.home) {
             HomeFeature()
@@ -55,18 +63,14 @@ struct AppFeature: Reducer {
                 state.viewState = .loaded(state.homeState)
 
             case let .verifyDeviceResponse(.failure(error)):
-                if case ConnectionError.banned = error {
-                    state.viewState = .failed(.banned)
-                    return .none
-                }
+                if case ConnectionError.banned = error { return banDevice(for: &state) }
                 if let commonError = (error as? GenericNetworkError)?.commonError, commonError == .deviceBanned {
-                    state.viewState = .failed(.banned)
-                    return .none
+                    return banDevice(for: &state)
                 }
                 state.viewState = .failed(.underlying(error))
                 
             case .deviceBanned:
-                state.viewState = .failed(.banned)
+                return banDevice(for: &state)
                 
             case .deviceNotEnrolled:
                 state.viewState = .failed(.delayed)
@@ -79,5 +83,14 @@ struct AppFeature: Reducer {
 
             return .none
         }
+    }
+}
+
+// MARK: - Private
+
+private extension AppFeature {
+    func banDevice(for state: inout State) -> Effect<Action> {
+        state.viewState = .failed(.banned)
+        return tunnelManagerSubscriber.stopVPN().map { result in Action.tunnelStopped }
     }
 }
